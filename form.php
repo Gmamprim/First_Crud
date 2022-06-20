2671539b98acdaed0667d0d0d92129ef944d80e2
Gabriel <gabriel_mamprim@hotmail.com>
2022-06-20 15:44:58 -0300

    Projeto Inicial PHP, CSS, HTML

--- form.php
+++ form.php
@@ -1,24 +1,4 @@
 <?php
-
-function conectarBancoDeDados()
-{
-}
-?>
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        My First CRUD
-    </title>
-</head>
-
-<body>
-    <pre>
-        <?php
         /*
             // ou # para comentar
             $ para declarar vareavel
@@ -27,7 +7,7 @@ function conectarBancoDeDados()
             != para comparações diferentes
             || alternativas
             &&
-        */
+        
         $x = 1;
         $y = 0;
 
@@ -39,9 +19,6 @@ function conectarBancoDeDados()
             echo "X é igual a Y ou X é maior ou igual a 1";
         } else {
             echo "nenhuma das validações anteriores";
-        }
+        }*/
+        
         ?>
-    </pre>
-</body>
-
-</html>
