943bda8af3898164c97581046ee198eda5020ed6
Gabriel <gabriel_mamprim@hotmail.com>
2022-06-20 16:46:30 -0300

    Form PHP introdução a conexão de Banco, MYSQLI e TRYCATCH

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('HOST_NAME', 'localhost');
+define('USER_NAME', 'gabriel');
+define('USER_PASSWORD', 'password');
+define('DATABASE', 'mmd');
--- form.php
+++ form.php
@@ -1,5 +1,6 @@
 <?php
-        /*
+
+/*
             // ou # para comentar
             $ para declarar vareavel
             = para atribuir valor
@@ -20,5 +21,40 @@
         } else {
             echo "nenhuma das validações anteriores";
         }*/
-        
-        ?>
+
+#incluindo o arquivo config.php        
+include('./config.php');
+
+#Atribuindo os valores do formulario para as vareaveis
+$name = $_POST['name'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+#Executando a query de inserção
+ExecutarQuery("INSERT INTO users(name,email,senha)VALUES('$name','$email','$senha')");
+
+#declaração da função
+function ExecutarQuery($query)
+{
+    try {
+        $con = new mysqli(HOST_NAME, USER_NAME, USER_PASSWORD, DATABASE);
+        if (!$con) {
+            echo "Não possivel conectar ao banco";
+            die();
+        }
+
+        $stmt = $con->prepare($query);
+        if (!$stmt) {
+            echo "Não possivel preparar a query";
+            die();
+        }
+
+        if (!$stmt->execute()) {
+            echo "erro ao executar a query";
+        }
+        echo "Query Executada com sucesso!.";
+        die();
+    } catch (Exception $e) {
+        echo "não foi possivel conectar ao banco.<br>" . $e->getMessage();
+    }
+}
